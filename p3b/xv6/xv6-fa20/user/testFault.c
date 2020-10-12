03109071352ab7acb522d5fabba7f3de8a23f1fa
garrett-low <usernamegl@live.com>
2020-10-11 20:58:11 -0500

    P3B: remove comments

--- p3b/xv6/xv6-fa20/user/testFault.c
+++ p3b/xv6/xv6-fa20/user/testFault.c
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[]) {
   int *p = 0;
   printf(1, "cause seg fault\n");
-  printf(1, "%d", *p);
+  printf(1, "%x", *p);
   
   exit();
 }
--- p3b/xv6/xv6-fa20/user/testFirstCodeAddr.c
+++ p3b/xv6/xv6-fa20/user/testFirstCodeAddr.c
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[]) {
   int *p = (int*) 0x2000;
   printf(1, "doesn't cause seg fault\n");
-  printf(1, "%d\n", *p);
+  printf(1, "%x\n", *p);
   
   exit();
 }
