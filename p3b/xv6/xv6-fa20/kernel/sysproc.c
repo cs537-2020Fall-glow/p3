c02735680834176f196c76f655826bc4f190ec17
Kevin Kokesh <kokesh@royal-23.cs.wisc.edu>
2020-10-22 19:24:37 -0500

    more comment removal

--- p3b/xv6/xv6-fa20/kernel/sysproc.c
+++ p3b/xv6/xv6-fa20/kernel/sysproc.c
@@ -51,7 +51,6 @@ sys_sbrk(void)
     return -1;
   // P3B - heap growing into stack
   if(proc->sz + n + (PGSIZE*5) > proc->stackLow) {
-    // cprintf("stackLow: %d, sz: %d, n: %d, heap will grow to: %d, 5 under stackLow: %d\n", proc->stackLow, proc->sz, n, proc->sz + n, proc->stackLow - (PGSIZE*5));
     return -1;
   }
   addr = proc->sz;
